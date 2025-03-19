bb9735d7d03188ae35e455eba6825c963455bb6f
christopherpandalee <christopherpandalee@gmail.com>
2025-03-18 20:07:29 -0400

    Made a practice package

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+
+setup(
+
+    name='myPackage',
+
+    version='0.1',
+
+    description='A simple demonstration package',
+
+    url='http://github.com/christopherpandalee/M09packagepractice',
+
+    author='Christopher Lee',
+
+    author_email='christopherpandalee@gmail.com',
+
+    license='MIT',
+
+    packages=['myPackage']
+
+)
